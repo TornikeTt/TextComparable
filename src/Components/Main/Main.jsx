35d8794423b3e7212226721ce6142b1b4a72f79a
Tornike <chargeishvilitornike@gmail.com>
2025-09-10 05:58:10 +0400

    Change class names and add proper responsive layout for main section

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,37 +1,41 @@
 import "./Main.scss";
-
-import newFileIcon from "../../assets/new-file-icon.png";
-import bidirectionalArrow from "../../assets/bidirectional arrow.png";
+import newComparisonIcon from "../../assets/new-file-icon.png";
+import comparisonArrowIcon from "../../assets/bidirectional arrow.png";
 
 function Main() {
     return (
         <main>
-            <div className="form-header">
-                <div className="form-language">
-                    <select name="language">
-                        <option value="ka">ქართული</option>
-                        <option value="en">English</option>
-                    </select>
+            <div className="container">
+                <div className="comparisonFormHeader">
+                    <div className="languageSelector">
+                        <select name="language">
+                            <option value="ka">ქართული</option>
+                            <option value="en">English</option>
+                        </select>
 
-                    <label className="form-checkbox">
-                        <input type="checkbox" />
-                        <span>ფორმატის შენარჩუნება</span>
-                    </label>
+                        <label>
+                            <input type="checkbox" />
+                            <span>ფორმატის შენარჩუნება</span>
+                        </label>
+                    </div>
+
+                    <button className="newComparisonButton">
+                        <img
+                            src={newComparisonIcon}
+                            alt="Start new comparison"
+                        />
+                        <span>ახლის გახსნა</span>
+                    </button>
                 </div>
 
-                <button className="new-form">
-                    <img src={newFileIcon} alt="Add new file" />
-                    <span>ახლის გახსნა</span>
-                </button>
-            </div>
+                <div className="textComparisonArea">
+                    <textarea placeholder="დაიწყე წერა..."></textarea>
+                    <img src={comparisonArrowIcon} alt="Compare text" />
+                    <textarea placeholder="დაიწყე წერა..."></textarea>
+                </div>
 
-            <div className="compareText_container">
-                <textarea placeholder="დაიწყე წერა..."></textarea>
-                <img src={bidirectionalArrow} />
-                <textarea placeholder="დაიწყე წერა..."></textarea>
+                <button className="compareButton">შედარება</button>
             </div>
-
-            <button className="compear_button">შედარება</button>
         </main>
     );
 }
